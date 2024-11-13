edaae9928924fabe53bec8a97bc08dfce40da843
Heren Özgün <hrnzgn@hotmail.com>
2024-11-13 16:41:17 +0300

    Added Dash (Will be improved)

--- HankMovement.gd
+++ HankMovement.gd
@@ -3,13 +3,16 @@ extends CharacterBody3D
 const SPEED = 8.0
 const JUMP_VELOCITY = 4.5
 const SENSITIVITY = 0.002
-const DASH_VELOCITY = 400
+const DASH_VELOCITY = 150.0
 const WALL_TILT = 0.5
 
 var JUMP_COUNT = 2
 var WALL_INTERACTION = 1
 var WALL_DETECTION = 1
-var EXTRA_VELOCITY = Vector3.ZERO
+var is_dashing = false
+var DASH_TIME = 0.2
+var DASH_TIMER = 0.0
+var dash_direction = Vector3.ZERO
 
 
 var SIDEWAYS_TILT = 0.0
@@ -21,11 +24,13 @@ var CURRENT_STATE = PlayerState.WalkingState
 @onready var RIGHT_RAYCAST = $RightRaycast;
 @onready var head = $Head;
 @onready var camera = $Head/FPSCam;
-@onready var dash = $ForwardRaycast;
 
 
 enum PlayerState { WalkingState, RunningState, MidAirState, DashState, OnWallState}
 
+
+	
+		
 
 func wall_detecion():
 	
@@ -60,6 +65,9 @@ func _unhandled_input(event):
 			
 func _physics_process(delta: float) -> void:
 	
+	var input_dir := Input.get_vector("left", "right", "up", "down")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if is_on_wall() and not is_on_floor() and WALL_INTERACTION > 0:
 		wall_detecion()
 		if Input.is_action_just_pressed("Jump"):
@@ -79,18 +87,30 @@ func _physics_process(delta: float) -> void:
 		JUMP_COUNT = 2
 		WALL_INTERACTION = 1;
 		
-	# Dash mechanic
-	#if Input.is_action_just_pressed("Dash"):
 	head.rotation_degrees.z = lerp(head.rotation_degrees.z, SIDEWAYS_TILT, 0.1)	
 	head.rotation_degrees.x = lerp(head.rotation_degrees.x, NORMAL_TILT, 0.1)	
 	
-	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:	
+	if direction:
+		
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 		NORMAL_TILT = head.transform.basis.z.dot(direction) * 3.5
 		SIDEWAYS_TILT = head.transform.basis.x.dot(direction) * -2.5
+			  
+		if is_dashing:
+			
+			DASH_TIMER -= delta
+			if DASH_TIMER > 0:
+				velocity.x = lerp(velocity.x, dash_direction.x * DASH_VELOCITY, 0.2)
+				velocity.z = lerp(velocity.z, dash_direction.z * DASH_VELOCITY, 0.2)
+			if DASH_TIMER <=0:
+				is_dashing = false
+		
+		if Input.is_action_just_pressed("Dash"):
+			is_dashing = true
+			DASH_TIMER = DASH_TIME
+			dash_direction = direction.normalized()
+			
 		
 	else:
 		velocity.x = 0.0
